83f3511092cc027fc21679e6fde75c426ae854f6
21120455 <hoai0455fit@gmail.com>
2025-05-03 14:31:42 +0700

    refactor: integrate add and delete category api

--- src/apis/categoryApi.tsx
+++ src/apis/categoryApi.tsx
@@ -3,22 +3,44 @@ import { CustomResponse } from "../types/common/CustomResponse";
 import { CategoryResponse } from "../types/category/CategoryResponse";
 import { CategoryRequest } from "../types/category/CategoryRequest";
 import { Category } from "../types/category/Category";
+
 const API_URL = import.meta.env.VITE_API_URL;
-export const getCategories = async (page: number, perpage: number): Promise<CategoryResponse> => {
-    const response = await axios.get<CustomResponse<CategoryResponse>>(`${API_URL}/categories`, {
-        params: {
-            page, perpage
-        },
-    });
-    if (!response.data.isSuccess || !response.data.data) {
-        throw new Error("Failed to fetch categories");
+export const getCategories = async (page: number, perpage: number): Promise<CustomResponse<CategoryResponse>> => {
+    try {
+        const response = await axios.get<CustomResponse<CategoryResponse>>(`${API_URL}/categories`, {
+            params: {
+                page, perpage
+            },
+        });
+        return response.data;
     }
-    return response.data.data;
+    catch (error: any) {
+        return error.response.data;
+    }
 }
 export const updateCategory = async (categoryId: string, categoryData: CategoryRequest): Promise<CustomResponse<Category>> => {
-    const response = await axios.put<CustomResponse<Category>>(`${API_URL}/categories/${categoryId}`, categoryData);
-    if (!response.data.isSuccess) {
-        throw new Error("Failed to update category");
+    try {
+        const response = await axios.put<CustomResponse<Category>>(`${API_URL}/categories/${categoryId}`, categoryData);
+        return response.data;
     }
-    return response.data;
+    catch (error: any) {
+        return error.response.data;
+    }
 }
+export const addCategory = async (categoryData: CategoryRequest): Promise<CustomResponse<Category>> => {
+    try {
+        const response = await axios.post<CustomResponse<Category>>(`${API_URL}/categories`, categoryData);
+        return response.data;
+    } catch (error: any) {
+        return error.response.data;
+    }
+}
+export const deleteCategory = async (categoryId: string): Promise<CustomResponse<Category>> => {
+    try {
+        const response = await axios.delete<CustomResponse<Category>>(`${API_URL}/categories/${categoryId}`);
+        return response.data;
+    } catch (error: any) {
+        return error.response.data;
+    }
+}
+
--- src/types/common/CustomResponse.tsx
+++ src/types/common/CustomResponse.tsx
@@ -2,5 +2,7 @@ export interface CustomResponse<T> {
     timestamp: string;
     httpStatus: string;
     isSuccess: boolean;
+    message: string;
+    subErrors?: unknown;
     data?: T;
 }
